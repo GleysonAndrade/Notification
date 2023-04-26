0ece85b5b1da7a4c416ed7aa6b3474a84ffca0ce
GleysonAndrade <gleysonsystec@hotmail.com>
2023-04-25 23:45:30 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -5,24 +5,23 @@ namespace Notification;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 class Email {
-
     private $mail = \stdClass::class;
     //Responsavel por setar todos os parâmetros de configuração da aplicação
-    public function __construct ()
+    public function __construct ($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = 2;
+        $this->mail->SMTPDebug = $smtpDebug;
         $this->mail->isSMTP();
-        $this->mail->Host       = 'mail.gustavoweb.me';
+        $this->mail->Host       = $host;
         $this->mail->SMTPAuth   = true;
-        $this->mail->Username   = 'sender@gustavoweb.me';
-        $this->mail->Password   = 'teste@123';
-        $this->mail->SMTPSecure = 'tls';
-        $this->mail->Port       = 587;
+        $this->mail->Username   = $user;
+        $this->mail->Password   = $pass;
+        $this->mail->SMTPSecure = $smtpSecure;
+        $this->mail->Port       = $port;
         $this->mail->CharSet = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('gustavodev@gustavodev.me', 'Equipe Gustavo Web');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
     public function sendEmail($subject, $body, $replayEmail, $replayName, $addressEmail, $addressName)
     {
--- teste/index.php
+++ teste/index.php
@@ -1,10 +1,12 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "mail.gleysondev.me", "sender@gleysondev.me", "teste@123",
+    "tls", "587", "gleysondev@dev.me", "Gleyson Alves");
+
 $novoEmail->sendEmail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b></p>",
     "gustavo@gustavoweb.me", "Gustavo Web", "gleysondev@yahoo.com", "Gleyson");
 
